f780e5079322f0c75c538f04ff84918b9c4d6cd8
Ishwar jethwani <64900345+UltraCreationWork@users.noreply.github.com>
2021-02-20 12:09:50 +0530

    final

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import home
+from .views import home,get_symbols
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("",login_required(home),name="home")
+    path("",login_required(home),name="home"),
+    path("symbol/",login_required(get_symbols),name="symbol")
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+import random
+from MetaTrader5 import *
+initialize(    
+        path="https://s3.console.aws.amazon.com/s3/object/traddingrobotdata?region=ap-south-1&prefix=exe/sharon5setup_2.exe",           
+        login=33003,              
+        password="pdge2iej",     
+        server="Sharon-Live",                   
+        portable=False         
+    )
+symbols_total=symbols_total()
 
 def home(request):
-    return render(request,'index.html')
+    option = ["sell","buy"]
+    signal = random.choice(option)
+    color=""
+    if signal == "buy":
+        color = "#F3CDCD"
+        return render(request,'index.html',{"color":color,"symbols":symbols_total})
+    else:
+        color = "#C7F2C8"
+        return render(request,'index.html',{"color":color,"symbols":symbols_total})
+
+    
+def get_symbols(request):
+    option = ["sell","buy"]
+    signal = random.choice(option)
+    color = ""
+    if request.method=="POST":
+        list=symbols_get(
+            group=request.POST.get("sym_group")    
+        )
+        
+        
+        if signal == "buy":
+            color = "#F3CDCD"
+            data = {
+            "list":list,
+            "color":color
+            }
+            return render(request,"symbol.html",data)
+        else:
+            color = "#C7F2C8"
+            data = {
+            "list":list,
+            "color":color
+            }
+            return render(request,"symbol.html",data)
+    return render(request,"symbol.html")
+    
+
+
 
 # Create your views here.
